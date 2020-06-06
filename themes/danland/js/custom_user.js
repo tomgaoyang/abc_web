41c698684265cbb34ddf61c7893b168062938562
Jacky Chan <chankongching@gmail.com>
2020-06-06 19:08:48 +0800

    Getting user name from page title

--- themes/danland/js/custom_user.js
+++ themes/danland/js/custom_user.js
@@ -11,14 +11,15 @@ $(function() {
   //    alert( 'JavaScript Loaded!')
   $(".field--name-field-user-validation").addClass("selected");
   $(".selected").hide();
-
+  var user = $("h1.page-title").html()
+  console.log("User = " + user)
   var url = 'https://0n2ravix4f.execute-api.ap-northeast-2.amazonaws.com/risk?name=jack'
   $("img").click(function() {
     fetch(url).then(function(response) {
       return response.json();
     }).then(function(data) {
       console.log('data = ' + data[1]);
-      if(data[1] != '0'){
+      if(data[1] == '0'){
         $(".field--name-field-user-validation").find(".field__item").html("通过审核 放贷成功");
         $(".field--name-field-user-validation").find(".field__item").css("color", "#3352FF");
 
