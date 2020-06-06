194078850f36dc6a0dfdc1aa6f7a2a8fad728711
Jacky Chan <chankongching@gmail.com>
2020-06-06 18:54:53 +0800

    try again

--- themes/danland/js/custom_user.js
+++ themes/danland/js/custom_user.js
@@ -18,10 +18,10 @@ $(function() {
       return response.json();
     }).then(function(data) {
       console.log('data = ' + data[1]);
-      if(int(data[1]) === 0){
+      if(!int(data[1])){
         $(".field--name-field-user-validation").find(".field__item").val("通过审核 放贷成功");
         $(".field--name-field-user-validation").find(".field__item").css("color", "#3352FF");
-        
+
       } else {
         $(".field--name-field-user-validation").find(".field__item").val("未通过审核");
         $(".field--name-field-user-validation").find(".field__item").css("color", "#e33e33");
