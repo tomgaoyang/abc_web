1adf5dc53a1e4550c7434d12728e57f50156e07b
Jacky Chan <chankongching@gmail.com>
2020-06-06 19:05:03 +0800

    change changing value method

--- themes/danland/js/custom_user.js
+++ themes/danland/js/custom_user.js
@@ -18,8 +18,8 @@ $(function() {
       return response.json();
     }).then(function(data) {
       console.log('data = ' + data[1]);
-      if(data[1] === '0'){
-        $(".field--name-field-user-validation").find(".field__item").val("通过审核 放贷成功");
+      if(data[1] == '0'){
+        $(".field--name-field-user-validation").find(".field__item").html("通过审核 放贷成功");
         $(".field--name-field-user-validation").find(".field__item").css("color", "#3352FF");
 
       } else {
