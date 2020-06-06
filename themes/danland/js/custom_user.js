6743ae7f2eca5448c7936cce8e620fe34e282390
chankongching <chankongching@gmail.com>
2020-06-06 09:35:34 +0000

    Removing merge conflict

--- themes/danland/js/custom_user.js
+++ themes/danland/js/custom_user.js
@@ -8,21 +8,12 @@
  */
  
 $(function() {
-<<<<<<< HEAD
-//    alert( 'JavaScript Loaded!' );
-$(".field--name-field-user-validation").addClass("selected"); 
-=======
 //    alert( 'JavaScript Loaded!')
     $(".field--name-field-user-validation").addClass("selected"); 
->>>>>>> e459439c2c0536b405a14d4724a2ddc47bfbb0e1
     $(".selected").hide();
     $( "img" ).click(function() {
 	  $(".selected").delay(500).show(0);
 	  $(".field--name-field-user-validation").find(".field__item").css("color","#e33e33");
 	  $(".field__item").find('img').hide();
      });
-<<<<<<< HEAD
-=======
-
->>>>>>> e459439c2c0536b405a14d4724a2ddc47bfbb0e1
 });
